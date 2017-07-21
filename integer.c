1296d2fd4fc0aa819f5d700e2e644e16977c99f5
HEMAMALINIsaravanan <hemamalini.sece@gmail.com>
2017-07-21 12:05:43 +0530

    Create integer.c

---
+++ integer.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a>0)
+printf("the number is positive");
+else if(a==0)
+printf("the number is zero");
+else
+printf("the number is negative");
+return 0;
+}
